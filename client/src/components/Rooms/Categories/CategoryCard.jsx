30413e43e84d8d9a4e14e6cb3808ca4ea8def54a
rafiulislamrana <irafiul802@gmail.com>
2024-03-09 14:14:48 +0600

    url string changed

--- client/src/components/Rooms/Categories/CategoryCard.jsx
+++ client/src/components/Rooms/Categories/CategoryCard.jsx
@@ -1,9 +1,24 @@
-
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string"
 
 const CategoryCard = ({lebel, icon:Icon}) => {
-    
+    const [params, setParams] = useSearchParams();
+    const navigate = useNavigate();
+    const handleClick = (name) => {
+        let currentQuery = {};
+        if(params){
+            currentQuery = qs.parse(params.toString( ));
+            const updatedQuery = {...currentQuery, category: name}
+            const url = qs.stringifyUrl({
+                url: "/",
+                query: updatedQuery,
+            })
+            navigate(url)
+        }
+    }
+    params.get("category");
     return (
-        <div className="flex flex-col items-center justify-center gap-2 border-b-2 cursor-pointer ">
+        <div onClick={() => handleClick(lebel)} className="flex flex-col items-center justify-center p-3 hover:text-neutral-800 transition gap-2 border-b-2 cursor-pointer ">
             <Icon size={26}></Icon>
             <div className=" text-sm font-medium">{lebel}</div>
         </div>
--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -16,7 +16,7 @@ const Rooms = () => {
     return (
         <div>
             <Container>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+                <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
                     {
                         rooms.map(room => (
                             <Card key={room._id} room={room}></Card>
